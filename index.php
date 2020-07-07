e156be7a4bc4a602621ff1917383f3e9df48f86f
antonsuteja <antonsutejapng@gmail.com>
2020-07-07 21:54:19 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <title>test up remote</title>
 </head>
 <body>
-<h1>hello world</h1>
-    
+    <h1>hello world</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet, deleniti!</p>
 </body>
 </html>
